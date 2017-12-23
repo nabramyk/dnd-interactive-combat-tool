02020677fed7d2b21dc5538b61ac07b0d37caec4
nabramyk <nabramyk@gmail.com>
2017-12-23 14:45:47 -0600

    Some code documentation, file restructuring, and additional functionality for highlighting cursor placement over the user's canvas

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//This server requires the 'express' NodeJS server framework
 var express = require('express');
 var app = express();
 
@@ -15,32 +16,59 @@ app.use(function(req, res, next) {
   next();
 });
 
+//Define the main path to index.html, which will be automatically loaded when the user visits for the
+//first time
+app.use(express.static('www'));
+
+//Define the path for where the javscript files are located for the users webpage
+app.use('/js', express.static(__dirname + '/www/js'))
+
+//Define the path for where the css stylesheets are located for the users webpage
+app.use('/css', express.static(__dirname + '/www/css'))
+
+//Initialize the array for centralizing the model between multiple users
 var cells = [];
 
-app.get('/', function (req,res) {
-	res.sendFile( __dirname + '/www/' + 'index.html');
-});
-
+/* Function for updating the users view when called
+ * 
+ * Recieves: a json array containing the list of elements which are currently displayed
+ * on that individual page
+ * 
+ * Sends: a vector of actions which the page will use to correct itself to the servers model
+ */
 app.post('/update', function (req,res) {
+	
+	//Parse the user call and store the list of live objects from the user in an array
 	var live_objects = JSON.parse(req.body.live_objects);	
+	
+	//Initialize the correction vector for returning the actions the webpage must take in
+	//order to be up to date with the server model
 	var correction_vector = [];
 	
+	//For each live element in the server model...
 	cells.find(function(el, ind, arr) {
+		//...if this live element does not exist in the user's grid...
 		if(live_objects.findIndex(function (el2) { return coordinate_comparison(el,el2); }) == -1) {
+			//...push to the correction vector so that it is added to the user's grid
 			correction_vector.push({"action":"add","item":el});
 		};
 	});
 	
+	//For each live element in the user's model...
 	live_objects.find(function(el, ind, arr) {
+		//...if this live element does not exist in the server's grid...
 		if(cells.findIndex(function (el2) { return coordinate_comparison(el,el2); }) == -1) {
+			//push to the correction vector so that it is removed from the user's grid
 			correction_vector.push({"action":"erase","item":el});
 		};
 	});
-		
+	
+	//Return the correction vector as a json array
 	res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify(correction_vector));
 });
 
+//Function for modifying the servers internal model of the grid board
 app.post('/push_change', function(req,res) {
 	var input = {color: req.body.color, x_coord: req.body.x_coord, y_coord: req.body.y_coord};
 	for(var i=0; i < cells.length; i++) {
@@ -56,6 +84,7 @@ app.post('/push_change', function(req,res) {
 	res.send(JSON.stringify(input));
 });
 
+//Main driver for booting up the server
 var server = app.listen(8080, function() {
 	var host = server.address().address
 	var port = server.address().port
