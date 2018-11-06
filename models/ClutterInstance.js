86e8bc8476407038f1a2815e79c16fb053e7b682
Nathan Abramyk <nathan.abramyk@pushinteractions.com>
2018-11-06 11:54:07 -0600

    Added a bunch of this statements.

--- models/ClutterInstance.js
+++ models/ClutterInstance.js
@@ -14,16 +14,16 @@ module.exports = class ClutterInstance {
 
     init() {
         return {
-			"grid_width": grid_space[0].width,
-			"grid_height": grid_space[0].height,
-			"elements": grid_space[0].elements,
-			"annotations": grid_space[0].annotations,
-			"spaces": grid_space.map((el) => { return { "id": el.id, "name": el.name } })
+			"grid_width": this.grid_space[0].width,
+			"grid_height": this.grid_space[0].height,
+			"elements": this.grid_space[0].elements,
+			"annotations": this.grid_space[0].annotations,
+			"spaces": this.grid_space.map((el) => { return { "id": el.id, "name": el.name } })
 		};
     }
 
     resizeHeight(msg) {
-		var temp = grid_space.find((el) => { return msg.grid_id == el.id });
+		var temp = this.grid_space.find((el) => { return msg.grid_id == el.id });
         temp.resizeHeight(msg.height);
         return {
 			"grid_id": msg.grid_id,
@@ -33,7 +33,7 @@ module.exports = class ClutterInstance {
     }
 
     resizeWidth(msg) {
-        var temp = grid_space.find((el) => { return msg.grid_id == el.id });
+        var temp = this.grid_space.find((el) => { return msg.grid_id == el.id });
         temp.resizeWidth(msg.width);
         return {
 			"grid_id": msg.grid_id,
@@ -43,7 +43,7 @@ module.exports = class ClutterInstance {
     }
 
     moveElement(msg) {
-		var movedElement = grid_space.find((el) => { return msg.grid_id == el.id }).nudgeElement(msg.x, msg.y, msg.direction);
+		var movedElement = this.grid_space.find((el) => { return msg.grid_id == el.id }).nudgeElement(msg.x, msg.y, msg.direction);
         if (typeof movedElement === 'undefined') 
             return undefined;
         else 
@@ -51,7 +51,7 @@ module.exports = class ClutterInstance {
     }
 
     warpElement(msg) {
-        var movedElement = grid_space.find((el) => { return msg.grid_id == el.id }).warpElement(msg.x, msg.y, msg.dest_x, msg.dest_y);
+        var movedElement = this.grid_space.find((el) => { return msg.grid_id == el.id }).warpElement(msg.x, msg.y, msg.dest_x, msg.dest_y);
         if (typeof movedElement === 'undefined') 
             return undefined;
         else 
@@ -73,60 +73,60 @@ module.exports = class ClutterInstance {
 			isUndefined(msg.name) ? "object" : msg.name,
 			msg.rotation);
 
-		return grid_space
+		return this.grid_space
 			.find((el) => { return el.id == msg.grid_id })
 			.addElementToGridSpace(input);
     }
 
     deleteElement(msg) {
-        grid_space.find((el) => { return el.id == msg.grid_id }).removeElementFromGridSpace(msg.element_id);
+        this.grid_space.find((el) => { return el.id == msg.grid_id }).removeElementFromGridSpace(msg.element_id);
     }
 
     editElement(msg) {
         msg.size = { "width" : JSON.parse(msg.size.width), "height" : JSON.parse(msg.size.height) };
 		msg.x = JSON.parse(msg.x);
         msg.y = JSON.parse(msg.y);
-        return grid_space.find((el) => { return el.id == msg.grid_id }).mutateElementInGridSpace(msg);
+        return this.grid_space.find((el) => { return el.id == msg.grid_id }).mutateElementInGridSpace(msg);
     }
 
     randomize(msg) {
-        return grid_space.find((el) => { return el.id == msg.grid_id }).generateRandomBoardElements();
+        return this.grid_space.find((el) => { return el.id == msg.grid_id }).generateRandomBoardElements();
     }
 
     reset(msg) {
-        grid_space.find((el) => { return el.id == msg.grid_id }).removeAllElementsFromGridSpace();
+        this.grid_space.find((el) => { return el.id == msg.grid_id }).removeAllElementsFromGridSpace();
         return { "grid_id": msg.grid_id };
     }
 
     createGridSpace() {
-        var newGridSpace = grid_space.push(new GridSpace(1, 1));
-        return { "id": grid_space[newGridSpace - 1].id, "name": grid_space[newGridSpace - 1].name };
+        var newGridSpace = this.grid_space.push(new GridSpace(1, 1));
+        return { "id": this.grid_space[newGridSpace - 1].id, "name": this.grid_space[newGridSpace - 1].name };
     }
 
     findGridSpace(msg) {
-        return grid_space.find((el) => { return el.id == msg.id; });
+        return this.grid_space.find((el) => { return el.id == msg.id; });
     }
 
     deleteGridSpace(msg) {
-        grid_space.splice(grid_space.indexOf(grid_space.find((el) => { return msg.grid_id == el.id; })), 1);
+        this.grid_space.splice(this.grid_space.indexOf(this.grid_space.find((el) => { return msg.grid_id == el.id; })), 1);
         return { "grid_id": msg.grid_id };
     }
 
     renameGrid(msg) {
-        grid_space.find((el) => { return el.id == msg.grid_id }).name = msg.grid_name;
+        this.grid_space.find((el) => { return el.id == msg.grid_id }).name = msg.grid_name;
         return msg;
     }
 
     addAnnotation(msg) {
-        return { "grid_id": msg.grid_id, "annotation": grid_space.find((el) => { return el.id == msg.grid_id }).addAnnotationToGridSpace(msg) };
+        return { "grid_id": msg.grid_id, "annotation": this.grid_space.find((el) => { return el.id == msg.grid_id }).addAnnotationToGridSpace(msg) };
     }
 
     deleteAnnotation(msg) {
-        return { "grid_id": msg.grid_id, "annotation_id": grid_space.find((el) => { return el.id == msg.grid_id }).removeAnnotationFromGridSpace(msg.annotation_id) };
+        return { "grid_id": msg.grid_id, "annotation_id": this.grid_space.find((el) => { return el.id == msg.grid_id }).removeAnnotationFromGridSpace(msg.annotation_id) };
     }
 
     undo(msg) {
-		var space = grid_space.find((el) => { return el.id == msg.grid_id });
+		var space = this.grid_space.find((el) => { return el.id == msg.grid_id });
 		var frame = space.historyUndo();
 		switch (frame.action) {
 			case "create":
@@ -138,7 +138,7 @@ module.exports = class ClutterInstance {
     }
 
     redo(msg) {
-		var frame = grid_space.find((el) => { return el.id == msg.grid_id }).historyRedo();
+		var frame = this.grid_space.find((el) => { return el.id == msg.grid_id }).historyRedo();
 		switch (frame.action) {
 			case "create": break;
 			case "edit": break;
